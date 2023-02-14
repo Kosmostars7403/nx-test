bc0a5ba413107ada3368d875a948ae1750439ac1
Ivan Chernyakov <3699818@mail.ru>
2023-02-14 19:19:39 +0300

    Optimize

--- apps/calendar/src/app/widgets/calendar/calendar.component.ts
+++ apps/calendar/src/app/widgets/calendar/calendar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DateTime } from "luxon";
+import {BehaviorSubject} from 'rxjs';
 
 const CALENDAR_ROWS_COUNT = 6
 const DAYS_IN_WEEK = 7
 const FIRST_DAY_OF_THE_WEEK = 2
+const MONTHS_IN_YEAR = 12
 
 @Component({
   selector: 'nx-test-calendar',
@@ -13,16 +15,22 @@ const FIRST_DAY_OF_THE_WEEK = 2
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss'],
 })
-export class CalendarComponent {
-  now = DateTime.now().setLocale('ru-RU')
-  month = 3
-  year = 2023
+export class CalendarComponent implements OnInit {
+  currentMonth = DateTime.now().setLocale('ru-RU')
+  month$ = new BehaviorSubject<{month: number, year: number}>({month: 11, year: 2023})
+
   sheet: DateTime[][] = []
 
-
   ngOnInit() {
-    const month = this.getMonthObject()
-    DateTime.now().weekdayLong
+    this.month$.subscribe(({month, year}) => {
+      this.currentMonth = DateTime.local(year, month).setLocale('ru-RU')
+      this.recalculateSheet(month, year)
+    })
+  }
+
+  recalculateSheet(monthNum: number, year: number) {
+    this.sheet = []
+    const month = this.getMonthObject(monthNum, year)
 
     for (let rowIndex = 0; rowIndex < CALENDAR_ROWS_COUNT; rowIndex++) {
       const row: DateTime[] = [];
@@ -41,16 +49,16 @@ export class CalendarComponent {
     }
   }
 
-  getMonthStartDaysOffset = (): number => {
-    const startMonthWeekday = DateTime.local(this.year, this.month, 1).weekday;
+  getMonthStartDaysOffset = (month: DateTime): number => {
+    const startMonthWeekday = DateTime.local(month.year, month.month, 1).weekday;
 
     return startMonthWeekday >= FIRST_DAY_OF_THE_WEEK
       ? startMonthWeekday - FIRST_DAY_OF_THE_WEEK
       : DAYS_IN_WEEK - (FIRST_DAY_OF_THE_WEEK - startMonthWeekday);
   };
 
-  getMonthObject(monthOffset = 0): DateTime {
-    return DateTime.local(this.year, this.month + monthOffset)
+  getMonthObject(month: number, year: number): DateTime {
+    return DateTime.local(year, month)
   }
 
   getDayFromMonthRowCol = ({rowIndex, colIndex, month}: {
@@ -61,20 +69,34 @@ export class CalendarComponent {
     let day =
       rowIndex * DAYS_IN_WEEK +
       colIndex -
-      this.getMonthStartDaysOffset();
-
+      this.getMonthStartDaysOffset(month);
 
     if (day > month.daysInMonth) {
       day -= month.daysInMonth;
-      month = this.getMonthObject(1);
+      month = month.plus({month:1})
     }
 
     if (day <= 0) {
-      month = this.getMonthObject(-1);
+      month = month.minus({month:1});
       day = month.daysInMonth + day;
     }
 
     return DateTime.local(month.year, month.month, day);
   };
 
+  changeMonth(increment: number) {
+    const {month, year} = this.month$.value
+
+    if (month + increment > MONTHS_IN_YEAR) {
+      this.month$.next({month: 1, year: year + 1})
+      return
+    }
+
+    if (month + increment < 0) {
+      this.month$.next({month: 12, year: year - 1})
+      return
+    }
+
+    this.month$.next({month: month + increment, year})
+  }
 }
